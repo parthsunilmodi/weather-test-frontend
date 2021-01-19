e594207585faee3b1f9a127e787a668d7af6237f
Parth Modi <parthmodi.developer@gmail.com>
2021-01-19 17:16:41 +0530

    feat: display chart with api data

--- src/pages/chart/index.js
+++ src/pages/chart/index.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Spin, Space } from 'antd';
 import FusionCharts from 'fusioncharts';
+import moment from 'moment';
 import charts from 'fusioncharts/fusioncharts.charts';
 import ReactFusioncharts from 'react-fusioncharts';
-// import chartActions from '../../store/chart/actions';
+import chartActions from '../../store/chart/actions';
 import { getWeatherData, getLoading } from '../../store/chart/selector';
 
 const dataSource = {
@@ -13,114 +14,35 @@ const dataSource = {
     subcaption: '(As per government records)',
     showvalues: '0',
     numvisibleplot: '12',
-    plottooltext: '<b>$dataValue</b> temp_f $label',
+    plottooltext: '<b>$dataValue</b> temperature in fahrenheit at $label',
     theme: 'fusion',
   },
-  categories: [
-    {
-      category: [
-        {
-          label: '12.00',
-        },
-        {
-          label: '1.00',
-        },
-        {
-          label: '2.00',
-        },
-        {
-          label: '3.00',
-        },
-        {
-          label: '4.00',
-        },
-        {
-          label: '5.00',
-        },
-        {
-          label: '6.00',
-        },
-        {
-          label: '7.00',
-        },
-        {
-          label: '8.00',
-        },
-        {
-          label: '9.00',
-        },
-        {
-          label: '10.00',
-        },
-        {
-          label: '11.00',
-        },
-        {
-          label: '12.00',
-        },
-      ],
-    },
-  ],
-  dataset: [
-    {
-      data: [
-        {
-          value: '43.2',
-        },
-        {
-          value: '41.7',
-        },
-        {
-          value: '40.1',
-        },
-        {
-          value: '39.6',
-        },
-        {
-          value: '39',
-        },
-        {
-          value: '38.5',
-        },
-        {
-          value: '37.4',
-        },
-        {
-          value: '36.5',
-        },
-        {
-          value: '35.4',
-        },
-        {
-          value: '36.9',
-        },
-        {
-          value: '38.1',
-        },
-        {
-          value: '39.6',
-        },
-        {
-          value: '41',
-        },
-      ],
-    },
-  ],
 };
 
 const Chart = () => {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const weatherData = useSelector(getWeatherData);
   const loading = useSelector(getLoading);
 
-  console.log('weather-----', weatherData);
+  const [chartData, setChartData] = useState(dataSource);
+
   useEffect(() => {
     charts(FusionCharts);
-    // const interval = setInterval(() => {
-    //   dispatch(chartActions.getWeatherData());
-    // }, 5000);
-    // return () => clearInterval(interval);
+    const interval = setInterval(() => {
+      dispatch(chartActions.getWeatherData());
+    }, 5000);
+    return () => clearInterval(interval);
   }, []);
+
+  useEffect(() => {
+    const category = (weatherData?.forecast || []).map(data => ({
+      label: moment(data.time).format('hh:mm'),
+    }));
+    const dataset = (weatherData?.forecast || []).map(data => ({
+      value: data.temp_f,
+    }));
+    setChartData({ ...chartData, categories: [{ category }], dataset: [{ data: dataset }] });
+  }, [weatherData]);
 
   if (loading) {
     return (
@@ -132,7 +54,7 @@ const Chart = () => {
 
   return (
     <div>
-      <ReactFusioncharts type="scrollline2d" width="70%" height="60%" dataFormat="JSON" dataSource={dataSource} />
+      <ReactFusioncharts type="scrollline2d" width="70%" height="60%" dataFormat="JSON" dataSource={chartData} />
     </div>
   );
 };
